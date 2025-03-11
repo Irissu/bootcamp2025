4f8ae7844a51d4d564cba0bf831c05253d6dd48b
Iris de la Mora <3631529+Irissu@users.noreply.github.com>
2025-03-11 15:57:28 +0100

    first commit of GildedRose Kata with some Test by me

--- GlidedRoseKata/src/main/java/com/gildedrose/GildedRose.java
+++ GlidedRoseKata/src/main/java/com/gildedrose/GildedRose.java
@@ -9,10 +9,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals("Aged Brie") // si no es Brie
+                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { // No es Backstage
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { // No es Sulfuras
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -38,6 +38,18 @@ class GildedRose {
 
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if(items[i].name.contains("Conjured")) {
+                if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1;
+                }
+                if (items[i].sellIn < 0 ) {
+                    items[i].quality = items[i].quality - 1;
+                }
+                if (items[i].quality < 0) {
+                    items[i].quality = 0;
+                }
             }
 
             if (items[i].sellIn < 0) {
--- GlidedRoseKata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GlidedRoseKata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
@@ -11,7 +12,39 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void testProductNameIsConjured() {
+        Item[] items = new Item[] { new Item("Conjured Space Cake", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(items[0].name.contains("Conjured"));
+    }
+
+    @Test
+    void testQualityDegradationOfConjuredItems() {
+        Item[] items = new Item[] {
+            new Item("Conjured Space Brownie", 3, 5),
+            new Item("Conjured Space Cake", 1, 10),
+            new Item("Conjured Split Banana", 0, 6)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals(3, items[0].quality);
+        assertEquals(8, items[1].quality);
+        assertEquals(2, items[2].quality);
+    }
+
+    @Test
+    void testCheckPositiveQualityOfConjuredItems(){
+        Item[] items = new Item[] { new Item("Conjured Space Cake", 0, 0) };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertTrue(items[0].quality >= 0);
+    }
 }
