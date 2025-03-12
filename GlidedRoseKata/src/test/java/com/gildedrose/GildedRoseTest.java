cd3ea2ab012790957bdba9df11a2ad31537c4d7d
Iris de la Mora <3631529+Irissu@users.noreply.github.com>
2025-03-12 09:28:30 +0100

    more test, pom fixed

--- GlidedRoseKata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GlidedRoseKata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,16 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.approvaltests.Approvals;
+
+
+import java.util.List;
+
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
+//    @Test
+//    void foo() {
+//        Item[] items = new Item[] { new Item("foo", 0, 0) };
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//        assertEquals("foo", app.items[0].name);
+//    }
 
     @Test
     void testProductNameIsConjured() {
@@ -46,5 +51,32 @@ class GildedRoseTest {
         app.updateQuality();
 
         assertTrue(items[0].quality >= 0);
+    }
+
+    @Test
+//	@Disabled
+    void snapshot() {
+        Item[] items = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20),
+            new Item("Aged Brie", 2, 0),
+            new Item("Elixir of the Mongoose", 5, 7),
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new Item("Conjured Mana Cake", 3, 6)
+        };
+
+        GildedRose app = new GildedRose(items);
+        var output = new StringBuilder();
+        output.append("day,name, sellIn, quality\n");
+        List.of(items).forEach(item -> output.append("0," + item + "\n"));
+        for (int i = 1; i <= 31; i++) {
+            app.updateQuality();
+            for(Item item: items)
+                output.append(i + "," + item + "\n");
+        }
+        Approvals.verify(output);
     }
 }
