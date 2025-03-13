585044240eb520200a7477e3fe0405adbd52aea0
Iris de la Mora <3631529+Irissu@users.noreply.github.com>
2025-03-13 09:44:56 +0100

    new project, hibernate configured, entities done

---
+++ catalogo-microservicios/src/main/java/com/example/catalogo_microservicios/config/AddPortsConfig.java
@@ -0,0 +1,24 @@
+package com.example.catalogo_microservicios.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AddPortsConfig {
+    @Bean
+    public WebServerFactoryCustomizer<TomcatReactiveWebServerFactory> additonalPortCustomizer() {
+        return factory -> {
+            factory.addAdditionalTomcatConnectors(createConnector(8081));
+            factory.addAdditionalTomcatConnectors(createConnector(8082));
+        };
+    }
+
+    private Connector createConnector(int port) {
+        Connector connector = new Connector(TomcatReactiveWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(port);
+        return connector;
+    }
+}
