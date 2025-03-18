ebc7a21bc36d3ebc773e736141b38215f3962682
Iris de la Mora <3631529+Irissu@users.noreply.github.com>
2025-03-18 11:59:12 +0100

    new spring batch project with fake users

---
+++ batchproject/src/test/java/com/example/batchproject/BatchprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
