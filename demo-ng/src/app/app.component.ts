cd939ba632b108bb9877b24f65447175207c2b46
Iris de la Mora <3631529+Irissu@users.noreply.github.com>
2025-03-28 12:33:28 +0100

    refactory

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoggerService } from '../lib/my-core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'demo-ng';
+  title = 'iris';
+
+  constructor(_out: LoggerService) {
+    _out.log('Esto es un log');
+    _out.info('Esto es un info');
+    _out.warn('Esto es un warn');
+    _out.error('Esto es un error');
+  }
 }
--- demo-ng/src/app/app.config.ts
+++ demo-ng/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ERROR_LEVEL, LoggerService } from '../lib/my-core';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), LoggerService, {provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL}]
 };
